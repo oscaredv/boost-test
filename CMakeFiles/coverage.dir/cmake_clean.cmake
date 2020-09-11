8a8e5e66dfdbe22929e451ca1eb00d6414ea1bb9
oscaredv <32863353+oscaredv@users.noreply.github.com>
2020-09-11 20:57:48 +0000

    Deploying to gh-pages from  @ 42842d4a238883af7374a8ff81251e3a61b8f4ed 🚀

---
+++ CMakeFiles/coverage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coverage"
+  "coverage"
+  "tests.info"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/coverage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /boost-test
+# Build directory: /boost-test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tests "tests")
+set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/boost-test/CMakeLists.txt;28;ADD_TEST;/boost-test/CMakeLists.txt;0;")
